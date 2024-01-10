34df4611cef2461f5b96ccf8bbafa8123ce78a0c
Talpalaru Andrei Georgiani <geandrei0208@gmail.com>
2024-01-10 13:02:16 +0200

    Added split line

--- app.py
+++ app.py
@@ -11,17 +11,8 @@ import pafy
 import cv2
 
 # Drawing bounding boxes over the picture. Unused probably
-def draw_bounding_box(img, xmin, ymin, xmax, ymax):
-    xmin = round(xmin)
-    ymin = round(ymin)
-    xmax = round(xmax)
-    ymax = round(ymax)
-
-    start_point = (xmin, ymin) 
-    end_point = (xmax, ymax) 
-    color = (255, 0, 0) 
-
-    cv2.rectangle(img, start_point, end_point, color, 1)
+def draw_line(img, x1, y1, x2, y2):
+    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
 def get_stream_capture(url):
     video = pafy.new(url, basic=True)
@@ -52,6 +43,31 @@ full_h = 360
 full_w = 640
 
 editing_mode = True
+split_line = False
+
+def process_results(results, flags):
+    stats = {
+        "all_cars": 0,
+        "above_line": 0,
+        "below_line": 0
+    }
+
+    if "line" in flags:
+        if results:
+            line_coords = flags["line_coords"]
+
+            for b in results[0].boxes.xyxy:
+                # if y1 box below line
+                if b[1] > line_coords[1]:
+                    stats["below_line"] += 1
+                
+                # if y2 box above line 
+                if b[3] < line_coords[1]:
+                    stats["above_line"] += 1
+
+
+    stats["all_cars"] = len(results[0])
+    return stats
 
 # Get the next frame on "every"
 def next_frame():
@@ -73,6 +89,11 @@ def next_frame():
         # cropped_frame = rotated_frame[int(h/3):int(h * 2 / 3), int(w/2):int(w * 6 / 7), :]
         cropped_frame = rotated_frame[h_begin: h_begin + h_dif, w_begin:w_begin + w_dif]
 
+        if split_line:
+            hc = cropped_frame.shape[0]
+            wc = cropped_frame.shape[1]
+            draw_line(cropped_frame, 0, int(hc / 2), wc, int(hc / 2))
+
         return cropped_frame
 
     full_frame = transform_frame(full_frame, rotation)
@@ -82,6 +103,7 @@ def next_frame():
     #   runtime. Check gradio Slider
 
     results = None
+    stats = None
     if not editing_mode:
         results = model.track(
             full_frame,
@@ -94,6 +116,14 @@ def next_frame():
             line_width=1,
             verbose=False,
             save=True
+        )
+
+        stats = process_results(
+            results,
+            {
+                "line": split_line,
+                "line_coords": (0, int(full_frame.shape[0] / 2), full_frame.shape[1], int(full_frame.shape[0] / 2))
+            }
         )
 
     # Skip frames since not every frame is needed and predicting
@@ -111,12 +141,19 @@ def next_frame():
             hour_frame
         ]
     else:
-        if results:
-            yield [
-                results[0].save_dir + "\\" + results[0].path, 
-                "Cars on this street: " + str(len(results[0])), 
-                hour_frame
-            ]
+        if results and stats:
+            if split_line:
+                yield [
+                    results[0].save_dir + "\\" + results[0].path, 
+                    f"Cars above line: {stats['above_line']}\nCars below line: {stats['below_line']}",
+                    hour_frame
+                ]
+            else:
+                yield [
+                    results[0].save_dir + "\\" + results[0].path, 
+                    f"Cars on selected street: {stats['all_cars']}", 
+                    hour_frame
+                ]
 
 def rotation_changed(rot):
     global rotation
@@ -151,38 +188,50 @@ def switch_editing_mode():
         gr.update(visible=editing_mode), 
         gr.update(visible=editing_mode), 
         gr.update(visible=editing_mode), 
-        gr.update(visible=editing_mode)
+        gr.update(visible=editing_mode),
     ]
+
+def switch_split_line():
+    global split_line
+    split_line = not split_line
 
 with gr.Blocks() as demo:
     editing = gr.Button("Editing Mode")
-    frame = gr.Image(height=200, width=300)
+    frame = gr.Image()
 
     with gr.Row():
         hour_frame = gr.Image()
         car_count = gr.Label()
 
-    rotation_slider = gr.Slider(0, 180, rotation, label="Rotation", visible=editing_mode)
-    rotation_slider.change(rotation_changed, [rotation_slider], None)
-    with gr.Row():
-        w_begin_slider = gr.Slider(0, 630, w_begin, label="Width Begin", visible=editing_mode)
-        w_begin_slider.change(wb_changed, [w_begin_slider], None)
-        w_dif_slider = gr.Slider(0, 630, w_dif, label="Width Difference", visible=editing_mode)
-        w_dif_slider.change(wd_changed, [w_dif_slider], None)
+    with gr.Column():
+        rotation_slider = gr.Slider(0, 180, rotation, label="Rotation", visible=editing_mode)
+        rotation_slider.change(rotation_changed, [rotation_slider], None)
+        with gr.Row():
+            w_begin_slider = gr.Slider(0, 630, w_begin, label="Width Begin", visible=editing_mode)
+            w_begin_slider.change(wb_changed, [w_begin_slider], None)
+            w_dif_slider = gr.Slider(0, 630, w_dif, label="Width Difference", visible=editing_mode)
+            w_dif_slider.change(wd_changed, [w_dif_slider], None)
 
-    with gr.Row():
-        h_begin_slider = gr.Slider(0, 350, h_begin, label="Height Begin", visible=editing_mode)
-        h_begin_slider.change(hb_changed, [h_begin_slider], None)
-        h_dif_slider = gr.Slider(0, 350, h_dif, label="Height Difference", visible=editing_mode)
-        h_dif_slider.change(hd_changed, [h_dif_slider], None)
+        with gr.Row():
+            h_begin_slider = gr.Slider(0, 350, h_begin, label="Height Begin", visible=editing_mode)
+            h_begin_slider.change(hb_changed, [h_begin_slider], None)
+            h_dif_slider = gr.Slider(0, 350, h_dif, label="Height Difference", visible=editing_mode)
+            h_dif_slider.change(hd_changed, [h_dif_slider], None)
+        
+        with gr.Row():
+            split_check = gr.Checkbox(value=split_line, label="Traffic Line Split")
 
-    editing.click(switch_editing_mode, None, [rotation_slider, w_begin_slider, w_dif_slider, h_begin_slider, h_dif_slider])
-    # Refresh the image and predictions very 0.5 seconds
-    #   Supposing video is 60 fps, then showing and skipping 30 frames every half a second
-    #   would not delay the show, but this is not taking into account
-    #   the time to predict and that every 0.5 I'm showing one frame and skipping 30, so 31 frames advance
-    # I need to find a way to show the real time. Maybe by cropping the time in
-    #   the bottom left corner and using it in the code
+    split_check.change(switch_split_line)
+    editing.click(switch_editing_mode, 
+                  None, 
+                  [
+                      rotation_slider, 
+                      w_begin_slider, 
+                      w_dif_slider, 
+                      h_begin_slider, 
+                      h_dif_slider,
+                  ]
+    )
 
     demo.load(next_frame, None, [frame, car_count, hour_frame], show_progress=True, every=1, queue=True)
 
